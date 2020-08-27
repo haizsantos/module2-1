89eb29cb64f130f0fcdfa3223f44b42cbbb71fb2
roxasabigail <abigailroxas.iics@ust.edu.ph>
2020-08-27 21:19:24 +0800

    venue address created

--- js/script.js
+++ js/script.js
@@ -3,7 +3,7 @@ function validate(){
     let start = document.getElementById("eventDate1").value;
     let end = document.getElementById("eventDate2").value;
     let post = document.getElementById("postDate").value;
-
+    let venue = document.getElementById("venueAddress").value;
     
     if(start > end){
         alert("Invalid Event Type.")
@@ -11,7 +11,7 @@ function validate(){
     if(post > start){
         alert("Invalid input in posting date. Kindly choose before event date or in between dates of event.");
     }
-    if (title != "" && start < end && post <= start){
+    if (title && venue != "" && start < end && post <= start){
         alert("You have successfully created the Event!");
     }else{
         alert("The Event is not created.");
